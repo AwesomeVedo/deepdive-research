f18f6709c8c2acfbecdc305c9bb1f69201c2b29b
Miguel Acevedo <39278733+AwesomeVedo@users.noreply.github.com>
2026-01-26 22:47:25 -0500

    Added editVent function

--- src/services/braindump.ts
+++ src/services/braindump.ts
@@ -16,7 +16,7 @@ export type VentItem = {
 export type Vent = {
   id: string;
   title: string;
-  description?: string;
+  description: string;
   items: VentItem[];
   isActive: boolean;
   createdAt: number;
@@ -143,4 +143,45 @@ export function createVent(title: string): CreateVentResult {
   const vents = listVents();
   saveVents([newVent, ...vents]);
   return { ok: true, vent: newVent }
+}
+
+export function editVent(id: string, patch: VentPatch): EditVentResult {
+
+  const nextPatch = { ...patch }; // copy patch contents to nextPatch
+  // Check if nextPatch title exists
+  if (nextPatch.title !== undefined) {
+    const t = nextPatch.title.trim();
+    if (!t) return { ok: false, error: "Vent title is required" };
+    nextPatch.title = t;
+  }
+  // Check if nextPatch description exists
+  if (nextPatch.description !== undefined) {
+    nextPatch.description = nextPatch.description.trim(); // trim description
+  }
+
+  const vents = listVents(); // grab all vents
+  const existingVent = vents.find((v) => v.id === id); // find match
+
+  // Return false if vent doesn't exist
+  if (!existingVent) {
+    return { ok: false, error: "Vent does not exist." };
+  }
+
+  // Create new Vent Object
+  const updated: Vent = {
+    ...existingVent,
+    ...nextPatch,
+    updatedAt: Date.now(),
+  }
+
+  // Create new Vents array with edited/updated Vent Object
+  const nextVents = vents.map((v) =>
+    v.id === id ? updated : v
+  );
+
+  // Save new Vents array
+  saveVents(nextVents);
+
+  return { ok: true, vent: updated }
+
 }
